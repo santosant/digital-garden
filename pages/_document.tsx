8733387726b3eb35b603c7b16d23241e32b51be6
Antonio Anderson <hiroto.anderson@gmail.com>
2022-06-19 05:28:26 -0300

    fixing a hydration error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document(props) {
+export default function Document() {
   return (
     <Html lang='en'>
       <Head></Head>
+      <title>@hirowf</title>
       <body className='bg-gray-100 dark:bg-[#141517] text-black dark:text-white'>
         <Main />
         <NextScript />
